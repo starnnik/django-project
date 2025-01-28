ea953c2d916ac6adf00fc25e62be0b72e21440f8
starnnik <l.d.50@mil.ru>
2025-01-28 18:24:09 +0300

    add templates

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage
+
+
+def post_list(request):
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
